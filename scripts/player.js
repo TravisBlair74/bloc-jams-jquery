182221823c9c8408902d4eb13412f41dcbb63866
Travis-Blair <travisblair74@gmail.com>
2017-10-30 16:45:28 -0400

    Time and volume control

--- scripts/player.js
+++ scripts/player.js
@@ -13,14 +13,16 @@ class Player {
   getTime() {
     return this.soundObject.getTime();
   }
-  
+
   playPause (song = this.currentlyPlaying) {
+    const totalTime = song.duration;
+    $('#time-control .total-time').text( player.prettyTime(totalTime) );
     if (this.currentlyPlaying !== song) {
       // Stop the currently playing sound file (even if nothing is playing)
       this.soundObject.stop();
       // Clear classes on the song that's currently playing
       this.currentlyPlaying.element.removeClass('playing paused');
-      
+
       // Update our currentlyPlaying and playState properties
       this.currentlyPlaying = song;
       this.playState = 'stopped';
@@ -37,12 +39,21 @@ class Player {
       this.currentlyPlaying.element.removeClass('playing').addClass('paused');
     }
   }
-  
+
   skipTo (percent) {
     if (this.playState !== 'playing') { return }
     this.soundObject.setTime( (percent / 100) * this.soundObject.getDuration() );
   }
-  
+
+  prettyTime (timeInSeconds) {
+    function pad(n) {
+    return (n < 10) ? ("0" + n) : n;
+    }
+    const minutes = Math.floor(timeInSeconds/60);
+    const seconds = Math.floor(timeInSeconds % 60);
+    return minutes + ":" + pad(seconds);
+  }
+
   setVolume (percent) {
     this.volume = percent;
     this.soundObject.setVolume(percent);
@@ -50,4 +61,3 @@ class Player {
 }
 
 const player = new Player();
-
